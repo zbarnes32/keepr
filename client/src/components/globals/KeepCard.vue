ffcc68b816d2851d4b8684bec253c13c3d46c85a
Zach Barnes <zachary.barnes.32@gmail.com>
2024-08-26 01:33:06 -0600

    Able to get the cards to show their image, name, and creator img. Needs styling.

--- client/src/components/globals/KeepCard.vue
+++ client/src/components/globals/KeepCard.vue
@@ -7,10 +7,34 @@ defineProps({ keep: {type: Keep, required: true} })
 
 
 <template>
-    <h1>{{  keep.name }}</h1>
+    <section>
+        <div class="shadow">
+            <div class="card bg-dark text-white">
+                <img :src="keep.img" class="card-img" :alt="keep.name">
+            <div class="card-img-overlay">
+                <p class="fs-4 card-name">{{ keep.name }}</p>
+                <img :src="keep.creator.picture" class="creator-picture">
+            </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 
 <style lang="scss" scoped>
 
+.card-name{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+}
+
+.creator-picture {
+    height: 5vh;
+    object-fit: cover;
+    border-radius: 50%;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+}
 </style>
