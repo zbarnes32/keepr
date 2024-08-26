61ab6988415d48e207568e378a16db252342a02d
Zach Barnes <zachary.barnes.32@gmail.com>
2024-08-26 09:35:25 -0600

    Created reusable modal

---
+++ client/src/components/globals/MainModal.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+defineProps({ modalId: String })
+
+</script>
+
+
+<template>
+
+    <!-- Button trigger modal -->
+<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade" :id="modalId" tabindex="-1" :aria-labelledby="`${modalId}Label`" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" :id="`${modalId}Label`">
+            <slot name="modalHeader"></slot>
+        </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <slot name="modalBody"></slot>
+      </div>
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> -->
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
