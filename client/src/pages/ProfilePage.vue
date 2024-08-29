de0665cf0db77461d1ba0d0dbba95536ced29944
Zach Barnes <zachary.barnes.32@gmail.com>
2024-08-29 14:57:02 -0600

    quick update

--- client/src/pages/ProfilePage.vue
+++ client/src/pages/ProfilePage.vue
@@ -7,7 +7,7 @@ import { profileService } from '@/services/ProfileService.js';
 import { vaultsService } from '@/services/VaultsService.js';
 import { logger } from '@/utils/Logger.js';
 import Pop from '@/utils/Pop.js';
-import { computed, onMounted, watch } from 'vue';
+import { computed, watch } from 'vue';
 import { useRoute } from 'vue-router';
 
 
